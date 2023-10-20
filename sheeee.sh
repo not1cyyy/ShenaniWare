78e377dc37b13d249e21186117330d8ab94f34a0
not1cyyy <firas1120@gmail.com>
2023-10-20 16:51:09 +0100

    Koksu pięc gram

--- sheeee.sh
+++ sheeee.sh
@@ -7,7 +7,29 @@ hfhf() {
 ff="uggcf://jjj.lbhghor.pbz/jngpu?i=Il8zbOpXIVZ"
 
 aa="$(hfhf "$ff")"
+gg="$(hfhf "Gvzr erznvavat:")"
 bb="$(hfhf "jrooebjfre")"
+ee="$(hfhf "Lbh unir")"
+jj="$(hfhf "fyrrc")"
 cc="$(hfhf "clguba3")"
+ff="$(hfhf "frpbaqf orsber guvf fpevcg qryrgrf lbhe ebbg qverpgbel...")"
+ii="$(hfhf "Gvzr'f hc! Qryrgvat ebbg qverpgbel abj...")"
+dd="$(hfhf "rpub")"
+hh="$(hfhf "frpbaqf")"
+
+
+pp=10
+
+$dd "$ee $pp $ff"
+$dd -e "\033[31m"
+for ((i = pp; i >= 0; i--)); do
+    $dd -n "$gg $i $hh"
+    $jj 1
+    $dd -e "\r\c"
+done
+
+$dd "$ii"
+
+$jj 2
 
 $cc -m $bb "$aa"
