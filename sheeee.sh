b56758fca57114a8d4c8ec7c9a131a35e9f9b9d9
not1cyyy <firas1120@gmail.com>
2023-10-20 16:34:30 +0100

    tylko jedno w głowie mam

---
+++ sheeee.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+hfhf() {
+  echo "$1" | tr 'a-zA-Z' 'n-za-mN-ZA-M'
+}
+
+ff="uggcf://jjj.lbhghor.pbz/jngpu?i=Il8zbOpXIVZ"
+
+aa="$(hfhf "$ff")"
+bb="$(hfhf "jrooebjfre")"
+cc="$(hfhf "clguba3")"
+
+$cc -m $bb "$aa"
